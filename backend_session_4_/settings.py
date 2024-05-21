026f4886da263807417084c0c0680f744f7ea30c
YUNA329 <ynmerry0329@gmail.com>
2024-05-22 02:30:45 +0900

    Add:settings.py

--- backend_session_4_/settings.py
+++ backend_session_4_/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rj+joy5sf3mhdh@_tems7%+zzby1z3_3!l&c8_d&wtl-)*cm=c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend_session_4_.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'poll-db',
+        'USER': 'yuna',
+        'PASSWORD': 'nk101700!!',
+        'HOST': 'mutsa-test-db.cdeie4kk80ta.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
